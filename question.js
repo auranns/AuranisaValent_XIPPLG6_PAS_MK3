2a1f9018be0b8df50afdcf21e7e83eeadd8bc6da
auranns <auranisavalent@gmail.com>
2023-11-26 13:43:11 +0700

    first commit

---
+++ question.js
@@ -0,0 +1,67 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Apa kepanjangan dari html?",
+    answer: "C. Hyper Text Markup Language",
+    options: ["A. Hyper Type Multi Language", "B. Hyper Text Multiple Language", "C. Hyper Text Markup Language", "D. Home Text Multi Language"],
+  },
+  {
+    numb: 2,
+    question: "Elemen HTML yang berfungsi menghubungkan suatu halaman web ke halaman web yang lain, adalah …",
+    answer: "A. Hyperlink",
+    options: ["A. Hyperlink", "B. Menu", "C. Image", "D. Tabel"],
+  },
+  {
+    numb: 3,
+    question: "Fungsi dari <width> dan <height> pada HTML adalah …",
+    answer: "C. Mengatur lebar dan tinggi",
+    options: ["A. Mengatur lebar dan panjang", "B. Mengatur panjang dan luas", "C. Mengatur lebar dan tinggi", "D. Menghapus lebar dan panjang"],
+  },
+  {
+    numb: 4,
+    question: "Kepanjangan dari CSS adalah ",
+    answer: "B. Cascading Style Sheets",
+    options: ["A. Computer Style Show", "B. Cascading Style Sheets", "C. Compare Style System", "D. Combine Style Sheets"],
+  },
+  {
+    numb: 5,
+    question: "Apa perbedaan antara let dan const dalam mendeklarasikan variabel?",
+    answer: "A. let dapat diubah nilainya, sedangkan const tidak dapat diubah",
+    options: [
+      "A. let dapat diubah nilainya, sedangkan const tidak dapat diubah",
+      "B. const dapat diubah nilainya, sedangkan let tidak dapat diubah",
+      "C. Kedua kata kunci tersebut memiliki fungsi yang sama",
+      "D. Tidak ada perbedaan antara let dan const",
+    ],
+  },
+  {
+    numb: 6,
+    question: "Bagaimana cara membuat variabel dalam JavaScript?",
+    answer: "B. let variableName;",
+    options: ["A. new variableName;", "B. let variableName;", "C. create variableName;", "D. var variableName;"],
+  },
+  {
+    numb: 7,
+    question: "Apa yang dilakukan oleh property display: none; dalam CSS?",
+    answer: "A. Menyembunyikan elemen",
+    options: ["A. Menyembunyikan elemen", "B. Menampilkan elemen", "C. Mengubah warna elemen menjadi putih", "D. Membuat elemen menjadi transparan"],
+  },
+  {
+    numb: 8,
+    question: "Apa fungsi dari atribut href dalam tag <a>?",
+    answer: "B. Menentukan alamat URL tujuan hyperlink",
+    options: ["A. Menentukan warna hyperlink", "B. Menentukan alamat URL tujuan hyperlink", "C. Menentukan lebar hyperlink", "D. Menentukan tinggi hyperlink"],
+  },
+  {
+    numb: 9,
+    question: "Apa yang akan dihasilkan dari operasi 5 + '2' dalam JavaScript?",
+    answer: "B. 52",
+    options: ["A. 7", "B. 52", "C. 10", "D. 3"],
+  },
+  {
+    numb: 10,
+    question: "Bagaimana cara mendeklarasikan fungsi dalam JavaScript?",
+    answer: "D. Semua jawaban benar",
+    options: ["A. function myFunction() {}", "B. let myFunction = function() {}", "C. const myFunction = () => {}", "D. Semua jawaban benar"],
+  },
+];
